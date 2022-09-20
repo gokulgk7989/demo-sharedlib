4d67ec40f37ccc05e4bc1f5eb1191a7d07c1858c
Gokul <gokulkumargk7989@gmail.com>
2022-09-20 10:36:53 +0530

    fixed spell mistake

---
+++ vars/tomcatdeployment.groovy
@@ -0,0 +1,6 @@
+def call(){
+    sshagent(['dev-tomcat']) {
+                  sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@172.31.41.206:/opt/tomcat9/webapps"
+                  sh "ssh ec2-user@172.31.41.206 /opt/tomcat9/bin/shutdown.sh"
+                  sh "ssh ec2-user@172.31.41.206 /opt/tomcat9/bin/startup.sh"
+}
