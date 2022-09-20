a303143eb1326f3a67242b77fc8cb33fb6b9a5da
gokulgk7989 <112245233+gokulgk7989@users.noreply.github.com>
2022-09-20 10:46:06 +0530

    Update tomcatdeployment.groovy

--- vars/tomcatdeployment.groovy
+++ vars/tomcatdeployment.groovy
@@ -1,7 +1,7 @@
-def call(){
-    sshagent(['dev-tomcat']) {
-                  sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@172.31.41.206:/opt/tomcat9/webapps"
-                  sh "ssh ec2-user@172.31.41.206 /opt/tomcat9/bin/shutdown.sh"
-                  sh "ssh ec2-user@172.31.41.206 /opt/tomcat9/bin/startup.sh"
+def call("ip","credid"){
+    sshagent([${credid}]) {
+        sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@${ip}:/opt/tomcat9/webapps"
+                  sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
+                  sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
     }
 }
